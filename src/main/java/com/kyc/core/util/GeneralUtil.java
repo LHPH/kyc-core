ec20330cef386d2eda21b8136946ddb66f03d7d5
LHPH <luislyly@hotmail.com>
2022-04-23 20:29:58 -0500

    Upgrade dependency and add more functions

--- src/main/java/com/kyc/core/util/GeneralUtil.java
+++ src/main/java/com/kyc/core/util/GeneralUtil.java
@@ -1,5 +1,7 @@
 package com.kyc.core.util;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -35,6 +37,25 @@ public final class GeneralUtil {
         return map != null && !map.isEmpty();
     }
 
+    public static <T,R> R getValue(T parent, Function<T,R> getter){
+
+        if(ObjectUtils.allNotNull(parent,getter)){
+            return getter.apply(parent);
+        }
+        return null;
+    }
+
+    public static Integer toInt(Double value, Integer defaultValue){
+
+        if(value != null){
+            return value.intValue();
+        }
+        return defaultValue;
+    }
+
+    public static Integer toInt(Double value){
+        return toInt(value,0);
+    }
 
     private GeneralUtil(){}
 }
